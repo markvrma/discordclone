291224c3780f7694e432246e1e502f7f83ce1a40
markvrma <markvrma@gmail.com>
2023-06-10 01:00:09 +0530

    rrr-rr-endering out of items compeleted

--- base/views.py
+++ base/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 # Create your views here.
-rooms = [
-    {'id':1,'name':'rock'},
-    {'id':2,'name':'avant'},
-    {'id':3,'name':'techno'},
-]
+# rooms = [
+#     {'id':1,'name':'rock'},
+#     {'id':2,'name':'avant'},
+#     {'id':3,'name':'techno'},
+# ]
 def home(request):
-    context = {'rooms':rooms}
+    context = {'rooms':Room.objects.all()}
     return render(request,'base/home.html',context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request,'base/room.html', context)
