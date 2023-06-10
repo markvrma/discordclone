aa4f2282a9faf57df228aebe2886ec5779e85999
markvrma <markvrma@gmail.com>
2023-06-11 01:00:53 +0530

    oh gee users can sherlock their search bars all they want now

--- base/views.py
+++ base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
+
+
 # Create your views here.
 # rooms = [
 #     {'id':1,'name':'rock'},
@@ -9,42 +12,58 @@ from .forms import RoomForm
 #     {'id':3,'name':'techno'},
 # ]
 def home(request):
-    context = {'rooms':Room.objects.all()}
-    return render(request,'base/home.html',context)
+
+    # search
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains = q) |
+        Q(description__icontains=q)
+        )
+    topics = Topic.objects.all()    
+    rooms_count = rooms.count()
+    context = {"rooms": rooms,"topics":topics, "rooms_count":rooms_count}
+ 
+    return render(request, "base/home.html", context)
+
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
-    return render(request,'base/room.html', context)
+    context = {"room": room}
+    return render(request, "base/room.html", context)
+
 
 def createRoom(request):
     form = RoomForm()
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect("home")
 
-    context = {'form':form}
-    return render(request,'base/room_form.html', context)
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
+
 
 def updateRoom(request, pk):
-    room = Room.objects.get(id = pk)
+    room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
 
-    if request.method == 'POST':
-        form = RoomForm(request.POST,instance=room)
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
         if form.is_valid():
             form.save()
-            return redirect('home')
-    
-    context = {'form':form}
-    return render(request,'base/room_form.html',context)
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
+
 
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
-    if request.method == 'POST':
+    if request.method == "POST":
         room.delete()
-        return redirect('home')
-    return render(request,'base/delete.html', {'obj':room})
+        return redirect("home")
+    return render(request, "base/delete.html", {"obj": room})
