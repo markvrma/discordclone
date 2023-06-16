5a7f678b40bcdf3720202b7846094b68abaaad10
markvrma <markvrma@gmail.com>
2023-06-16 18:58:36 +0530

    Create/Update rooms fixed, yay

--- base/views.py
+++ base/views.py
@@ -119,17 +119,30 @@ def userProfile(request, pk):
 @login_required(login_url="login")
 def createRoom(request):
     form = RoomForm()
-
+    topics = Topic.objects.all()
     if request.method == "POST":
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            # form.save()
-            return redirect("home")
+        topic_name = request.POST.get('topic')
+        # wiil return topic or creates the topic
+        topic,created = Topic.objects.get_or_create(name=topic_name)
 
-    context = {"form": form}
+        # new way to save form because now we have to create a new jtopuc with the user
+        Room.objects.create(
+            host = request.user,
+            topic = topic,
+            name = request.POST.get('name'),
+            description = request.POST.get('description'),
+        )
+
+        # how we saved the form previously
+        # form = RoomForm(request.POST)
+        # if form.is_valid():
+        #     room = form.save(commit=False)
+        #     room.host = request.user
+        #     room.save()
+            # form.save()
+        return redirect("home")
+
+    context = {"form": form,"topics":topics}
     return render(request, "base/room_form.html", context)
 
 
@@ -137,17 +150,21 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse("You are not allowed here")
 
     if request.method == "POST":
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect("home")
+        topic_name = request.POST.get('topic')
+        # wiil return topic or creates the topic
+        topic,created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect("home")
 
-    context = {"form": form}
+    context = {"form": form,"topics":topics, "room":room}
     return render(request, "base/room_form.html", context)
 
 
